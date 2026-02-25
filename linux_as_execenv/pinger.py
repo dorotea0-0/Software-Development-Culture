7fe56ee6356688995763a45db9a27f9b57aeda38
root <root@DESKTOP-OPGGP8H.localdomain>
2026-02-25 16:30:41 +0800

    Refactor, made variables name clean

--- linux_as_execenv/pinger.py
+++ linux_as_execenv/pinger.py
@@ -2,10 +2,10 @@ import requests
 from datetime import datetime, timedelta
 
 url_template = "https://simurg.space/gen_file?data=obs&date={date}"
-now = datetime.now()
+current = datetime.now()
 
 while True:
-    date = now.strftime("%Y-%m-%d")
+    date = current.strftime("%Y-%m-%d")
     url = url_template.format(date=date)
 
     response = requests.get(url=url, stream=True)
@@ -14,4 +14,4 @@ while True:
         print(f"last available date: {date}")
         break
     else:
-        now = now - timedelta(days=1)
+        current = current - timedelta(days=1)
