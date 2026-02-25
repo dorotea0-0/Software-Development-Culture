82c0d0483e8241996d96a589ba0bb739f9ddd858
root <root@DESKTOP-OPGGP8H.localdomain>
2026-02-25 16:18:40 +0800

    прописали дату сейчас

---
+++ linux_as_execenv/pinger.py
@@ -0,0 +1,10 @@
+import requests
+from datetime import datetime
+
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
+now = datetime.now()
+date=now.strftime("%Y-%m-%d")
+url=url_temlate.format(date=date)
+
+response = requests.get(url=url, stream=True)
+print(f"For {date} got: ", response)
