4cbec93fe001b5ce648bb294f1f37790ee78b7f7
root <root@DESKTOP-OPGGP8H.localdomain>
2026-03-04 16:29:41 +0800

    Показали перегруженный тест

--- testing/tests/math_principles.py
+++ testing/tests/math_principles.py
@@ -21,7 +21,14 @@ def test_dublicate():
     #тест дублирует себя
     assert add(2, 2) == 2 + 2
 
+def test_overcomplicate():
+    #Слишком перегруженный тест
+    for i in range(0,2**32):
+        for j in range(0,2**32):
+            assert add(i, i) == sum(i, j)
+
 if __name__ == "__main__":
     test_addition()
     test_with_bug()
     test_dublicate()
+    #test_overcomplicate()
