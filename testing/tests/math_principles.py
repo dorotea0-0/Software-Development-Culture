5cfb2895287990adbbe108a0151cae115c5efc40
root <root@DESKTOP-OPGGP8H.localdomain>
2026-03-04 16:13:51 +0800

    Show test-not-grant-you-from-error principle

--- testing/tests/math_principles.py
+++ testing/tests/math_principles.py
@@ -1,11 +1,22 @@
 import sys
 sys.path.append("../src")
 
-from math_demo import add
+from math_demo import (
+add,
+add_with_bug
+)
 
 def test_addition():
     assert add(2, 2) == 4
     print("Success")
 
+def test_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    assert add_with_bug(1, 1) == 1
+    print("Bug")
+    #assert add_with_bug(2, 3) == 5
+
 if __name__ == "__main__":
     test_addition()
+    test_with_bug()
